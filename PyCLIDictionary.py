226bfd9b377b7ba22ac8ae183249cb928b3eef87
Aryaman73 <aryaman.singh.2012@gmail.com>
2020-04-19 18:12:34 -0400

    Implement Urban API, randomly selecting either

--- PyCLIDictionary.py
+++ PyCLIDictionary.py
@@ -1,21 +1,44 @@
 #94 02:32
+import sys
+import requests
+import random
 import json
 from difflib import get_close_matches #for comparing strings!
 
 data = json.load(open("data.json"))
 
-def findWordInDict(word):
+# Urban Dictionary API
+url = "https://mashape-community-urban-dictionary.p.rapidapi.com/define"
+
+headers = {
+    'x-rapidapi-host': "mashape-community-urban-dictionary.p.rapidapi.com",
+    'x-rapidapi-key': "f7c2d6c1admshf9511bb1a414560p12dbdbjsn06ce2a38118a"
+    }
+
+def findWordInUrban(word):
+    querystring = {"term": word}
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    responseDict = json.loads(response.text)
+    return (responseDict['list'][0]['definition'])
+
+def findWordInData(word):
+    return data[word]
+
+def findWord(word):
+    fns = [findWordInData, findWordInUrban]
     if word in data:
-        return data[word]
+        return random.choice(fns)(word)
     elif len(get_close_matches(word, data.keys(), cutoff= 0.75)) > 0:
-        choice = input("Did you mean %s instead? Enter 'Y' or 'N': " % get_close_matches(word, data.keys(), cutoff= 0.75)[0]).upper()
-        if choice == "Y":
-            findWordIndict(get_close_matches(word, data.keys(), cutoff= 0.75)[0])
+        c = input("Did you mean %s instead? Enter 'Y' or 'N': " % get_close_matches(word, data.keys(), cutoff= 0.75)[0]).upper()
+        if c == "Y":
+            return random.choice(fns)(get_close_matches(word, data.keys(), cutoff= 0.75)[0])
+        else:
+            return "Oops! Try Again."
+    else:  
+        return "Word not found, please try another word. "
 
-
-    else:
-        return "Word not found :( "
-
-userInput = input("Enter a word: ").lower()
-
-print(findWordInDict(userInput))
+while(True):   
+    userInput = input("Enter a word: ").lower()
+    print(findWord(userInput))
+    if (input("Enter 'E' to exit: ").upper() == 'E'):
+        sys.exit()

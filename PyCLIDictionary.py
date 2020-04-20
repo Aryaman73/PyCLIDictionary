401692fc90d73e6f11df88cbe34ae2af9087cda4
Aryaman73 <aryaman.singh.2012@gmail.com>
2020-04-19 20:14:01 -0400

    Basic GUI done, no similar word support

--- PyCLIDictionary.py
+++ PyCLIDictionary.py
@@ -4,6 +4,7 @@ import requests
 import random
 import json
 from difflib import get_close_matches #for comparing strings!
+from tkinter import *
 
 data = json.load(open("data.json"))
 
@@ -24,21 +25,42 @@ def findWordInUrban(word):
 def findWordInData(word):
     return data[word]
 
-def findWord(word):
+# def findWord2():
+#     lbl.configure(text= txt.get())
+
+def findWord():
     fns = [findWordInData, findWordInUrban]
+    word = txt.get()
     if word in data:
-        return random.choice(fns)(word)
+        lbl.configure(text = random.choice(fns)(word))
     elif len(get_close_matches(word, data.keys(), cutoff= 0.75)) > 0:
         c = input("Did you mean %s instead? Enter 'Y' or 'N': " % get_close_matches(word, data.keys(), cutoff= 0.75)[0]).upper()
         if c == "Y":
-            return random.choice(fns)(get_close_matches(word, data.keys(), cutoff= 0.75)[0])
+            lbl.configure(text = random.choice(fns)(get_close_matches(word, data.keys(), cutoff= 0.75)[0]))
         else:
-            return "Oops! Try Again."
+            lbl.configure(text = "Oops! Try Again.")
     else:  
-        return "Word not found, please try another word. "
+        lbl.configure(text = "Word not found, please try another word. ")
+    
 
-while(True):   
-    userInput = input("Enter a word: ").lower()
-    print(findWord(userInput))
-    if (input("Enter 'E' to exit: ").upper() == 'E'):
-        sys.exit()
+# while(True):   
+#     userInput = input("Enter a word: ").lower()
+#     print(findWord(userInput))
+#     if (input("Enter 'E' to exit: ").upper() == 'E'):
+#         sys.exit()
+
+
+window = Tk()
+window.title("Reliable Dictionary")
+window.geometry('500x250')
+
+lbl = Label(window, text="Hello")
+lbl.grid(column=0, row=1)
+
+txt = Entry(window,width=10)
+txt.grid(column=1, row=0)
+
+btn = Button(window, text="Find Definitions", command= findWord)
+btn.grid(column=2, row=0)
+
+window.mainloop()

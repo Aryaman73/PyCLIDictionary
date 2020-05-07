6e44287ef8280c41422831740a7a1e6cc6bcfd33
Aryaman73 <aryaman.singh.2012@gmail.com>
2020-05-07 19:25:26 -0400

    Fixed colour issues and text wrapping

--- PyCLIDictionary.py
+++ PyCLIDictionary.py
@@ -24,43 +24,34 @@ def findWordInUrban(word):
     querystring = {"term": word}
     response = requests.request("GET", url, headers=headers, params=querystring)
     responseDict = json.loads(response.text)
-    return (responseDict['list'][0]['definition'])
+    print("Urban")
+    return (responseDict['list'][0]['definition'].replace('[', '').replace(']', ''))
 
 def findWordInData(word):
+    print("Non-Urban")
     return data[word]
-
-# def findWord2():
-#     lbl.configure(text= txt.get())
 
 def findWord():
     fns = [findWordInData, findWordInUrban]
     word = txt.get()
 
-    window2 = Tk()
-    window2.title(word)
-    window2.geometry('500x250')
+    # window2 = Tk()
+    # window2.title(word)
+    # window2.geometry('500x250')
 
-    lbl2 = Label(window2, text="Definition appears here!", wraplength = 500)
-    lbl2.grid(column=0, row=0, columnspan = 100)
+    # lbl2 = Label(window2, text="Definition appears here!", wraplength = 500)
+    # lbl2.grid(column=0, row=0, columnspan = 100)
 
     if word in data:
-        lbl2.configure(text = random.choice(fns)(word))
+        lbl.configure(text = random.choice(fns)(word))
     elif len(get_close_matches(word, data.keys(), cutoff= 0.75)) > 0:
-        c = input("Did you mean %s instead? Enter 'Y' or 'N': " % get_close_matches(word, data.keys(), cutoff= 0.75)[0]).upper()
+        c = "Y" # input("Did you mean %s instead? Enter 'Y' or 'N': " % get_close_matches(word, data.keys(), cutoff= 0.75)[0]).upper()
         if c == "Y":
-            lbl2.configure(text = random.choice(fns)(get_close_matches(word, data.keys(), cutoff= 0.75)[0]))
+            lbl.configure(text = random.choice(fns)(get_close_matches(word, data.keys(), cutoff= 0.75)[0]))
         else:
-            lbl2.configure(text = "Oops! Try Again.")
+            lbl.configure(text = "Oops! Try Again.")
     else:  
-        lbl2.configure(text = "Word not found, please try another word. ")
-    
-
-# while(True):   
-#     userInput = input("Enter a word: ").lower()
-#     print(findWord(userInput))
-#     if (input("Enter 'E' to exit: ").upper() == 'E'):
-#         sys.exit()
-
+        lbl.configure(text = "Word not found, please try another word. ")
 
 window = Tk()
 window.title("Reliable Dictionary")
@@ -72,10 +63,10 @@ logoPhoto = logoPhoto.resize((240, 100), Image.ANTIALIAS)
 photoImg =  ImageTk.PhotoImage(logoPhoto)
 
 
-logo = Label(window, image = photoImg)
+logo = Label(window, image = photoImg, background = "white")    
 logo.grid(column = 0, row = 1, columnspan = 100, rowspan = 5, pady = 1, padx = 125, sticky = "wens")
 
-lbl = Label(window, text="Definition appears here!")
+lbl = Label(window, text="the label appears here!", wraplength=480, background = "white")
 lbl.grid(column=0, row=7, columnspan = 100)
 
 window.grid_columnconfigure((0, 1, 2), weight=1)
